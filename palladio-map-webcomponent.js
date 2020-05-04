06761c04fcd518a74b845cdae75125d6259994de
Simon Wiles <simonjwiles@gmail.com>
2020-05-03 22:15:26 -0700

    Add basic functionality

--- palladio-map-webcomponent.js
+++ palladio-map-webcomponent.js
@@ -5,11 +5,78 @@ window.customElements.define(
   class extends PalladioWebComponentAbstractBase {
     constructor() {
       super();
-      this.stylesheets = [
-        "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css",
-        "palladio-map-webcomponent.css",
-      ];
-      this.scripts = ["https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"]
+      this.stylesheets = ["https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"];
+      this.scripts = ["https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"];
+
+      this.mapMap = {
+        "cesta.hd9ak6ie": "mapbox/light-v9",
+      };
+
+      this.mapConfig = {
+        center: [45.464, 9.1916],
+        zoom: 3,
+        minZoom: 2,
+        maxZoom: 20,
+        accessToken:
+          "pk.eyJ1IjoiY2VzdGEiLCJhIjoiMFo5dmlVZyJ9.Io52RcCMMnYukT77GjDJGA",
+      };
+    }
+
+    initMap() {
+      this.element = this.body.querySelector("div.map-view");
+
+      this.map = L.map(this.element).setView(
+        new L.LatLng(...this.mapConfig.center),
+        this.mapConfig.zoom,
+      );
+
+      this.map.attributionControl.addAttribution(
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      );
+      L.control.scale().addTo(this.map);
+    }
+
+    addTileSets(tileSets) {
+      // iterate tile set layers in reverse order
+      // (see palladio-map-view.js:1138)
+      [...tileSets].reverse().forEach((tileSet, i) => {
+        const layer = L.tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+          {
+            maxZoom: this.mapConfig.maxZoom,
+            id: this.mapMap[tileSet.mbId],
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: this.mapConfig.accessToken,
+          },
+        ).addTo(this.map);
+      });
+    }
+
+    addLayers(layers, rows) {
+      layers.forEach((layer) => {
+        if (layer.layerType === "data") {
+          rows.forEach((row) => {
+            L.circle(row[layer.mapping.sourceCoordinatesKey].split(","), {
+              color: layer.color,
+              fillColor: layer.color,
+              fillOpacity: 0.5,
+              radius: 100,
+            })
+              .bindPopup(row[layer.descriptiveDimKey])
+              .addTo(this.map);
+          });
+
+          // this needs to be more sophisticated if there are multiple data layers
+          this.map.fitBounds(
+            rows.map((row) =>
+              row[layer.mapping.sourceCoordinatesKey].split(","),
+            ),
+          );
+        }
+      });
     }
 
     render(data) {
@@ -37,6 +104,21 @@ window.customElements.define(
         `);
       }
 
+      const view = document
+        .createRange()
+        .createContextualFragment(`<div class="map-view"></div>`);
+
+      this.body.innerHTML = "";
+      this.body.appendChild(view);
+      this.body.querySelector("div.map-view").style.height = "400px";
+
+      this.scriptsReady.then(() => {
+        this.initMap();
+        if ("tileSets" in settings) {
+          this.addTileSets(settings.tileSets);
+          this.addLayers(settings.layers, rows);
+        }
+      });
     }
-  }
+  },
 );
